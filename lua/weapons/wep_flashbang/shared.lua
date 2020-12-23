077103b877ffff4365c6bc3663981e49474e68bf
Clem-Fern <clemfern456@gmail.com>
2020-12-23 14:24:20 +0100

    Grenades weapon code cleanup

--- lua/weapons/wep_flashbang/shared.lua
+++ lua/weapons/wep_flashbang/shared.lua
@@ -17,14 +17,12 @@ SWEP.Grenade = "ent_flashbang_proj"
 SWEP.Spawnable = true
 SWEP.AdminSpawnable = true
 
-SWEP.ViewModel	= "models/weapons/cstrike/c_eq_flashbang.mdl"
+SWEP.UseHands = true
+SWEP.ViewModel = "models/weapons/cstrike/c_eq_flashbang.mdl"
 SWEP.WorldModel = "models/weapons/w_eq_flashbang.mdl"
 
-SWEP.AutoSwitchFrom		= true
-
-SWEP.DrawCrosshair		= false
-
-SWEP.LimitedStock = false
+SWEP.AutoSwitchFrom	= true
+SWEP.DrawCrosshair = false
 
 function SWEP:GetGrenadeName()
    return "ent_flashbang_proj"
--- lua/weapons/wep_smokegrenade/shared.lua
+++ lua/weapons/wep_smokegrenade/shared.lua
@@ -17,15 +17,12 @@ SWEP.Grenade = "ent_smokegrenade_proj"
 SWEP.Spawnable = true
 SWEP.AdminSpawnable = true
 
-SWEP.UseHands           = true
-SWEP.ViewModel          = "models/weapons/cstrike/c_eq_smokegrenade.mdl"
-SWEP.WorldModel         = "models/weapons/w_eq_smokegrenade.mdl"
+SWEP.UseHands = true
+SWEP.ViewModel = "models/weapons/cstrike/c_eq_smokegrenade.mdl"
+SWEP.WorldModel = "models/weapons/w_eq_smokegrenade.mdl"
 
-SWEP.AutoSwitchFrom		= true
-
-SWEP.DrawCrosshair		= false
-
-SWEP.LimitedStock = false
+SWEP.AutoSwitchFrom	= true
+SWEP.DrawCrosshair = false
 
 function SWEP:GetGrenadeName()
    return "ent_smokegrenade_proj"
